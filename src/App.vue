85743fe88501bb41d1d7027b5301747a3a1130d4
Negio <1286553133@qq.com>
2021-05-18 15:43:09 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <FooterGuide v-if="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+<style lang="stylus"></style>
+<script>
+import FooterGuide from './components/FootGuide/FootGuide'
+export default {
+
+  components: {
+
+    FooterGuide
+  }
+}
+</script>
